04aeb5f625129e7dc83be1421e9f1cdc73453677
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-01-16 20:28:57 +0700

    lesson-17 create function for add cart to state in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,21 +2,26 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
-  cartItems: [
-    // {
-    //   id: 1,
-    //   name: "Books 1",
-    //   count: 3,
-    //   total: 150,
-    // },
-    // {
-    //   id: 2,
-    //   name: "Books 2",
-    //   count: 2,
-    //   total: 70,
-    // },
-  ],
+  cartItems: [],
   orderTotal: 220,
+};
+
+const updateCartItems = (cartItems, item, idx) => {
+  if (idx === -1) {
+    return [...cartItems, item];
+  } else {
+    return [...cartItems.slice(0, idx), item, ...cartItems.slice(idx + 1)];
+  }
+};
+
+const updateCartItem = (book, item = {}) => {
+  const { id = book.id, count = 0, title = book.title, total = 0 } = item;
+  return {
+    id,
+    title,
+    count: count + 1,
+    total: total + book.price,
+  };
 };
 
 const reducer = (state = initialState, action) => {
@@ -29,6 +34,7 @@ const reducer = (state = initialState, action) => {
         loading: true,
         error: null,
       };
+
     case "FETCH_BOOKS_SUCCESS":
       return {
         ...state,
@@ -36,6 +42,7 @@ const reducer = (state = initialState, action) => {
         loading: false,
         error: null,
       };
+
     case "FETCH_BOOKS_FAILURE":
       return {
         ...state,
@@ -43,17 +50,19 @@ const reducer = (state = initialState, action) => {
         loading: false,
         error: action.payload,
       };
+
     case "BOOK_ADDED_TO_CART":
       const bookId = action.payload;
       const book = state.books.find((book) => book.id === bookId);
-      const newItem = {
-        id: book.id,
-        name: book.title,
-        count: 1,
-        total: book.price,
-      };
+      const itemIndex = state.cartItems.findIndex(({ id }) => id === bookId);
+      const item = state.cartItems[itemIndex];
 
-      return { ...state, cartItems: [...state.cartItems, newItem] };
+      const newItem = updateCartItem(book, item);
+
+      return {
+        ...state,
+        cartItems: updateCartItems(state.cartItems, newItem, itemIndex),
+      };
     //
     default:
       return state;
