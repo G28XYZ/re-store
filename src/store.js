40a9aa9e0d54e9729b73d49aea11059e50c7cccb
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-01-16 22:21:10 +0700

    lesson-21 create middleware replace enhancer in store

--- src/store.js
+++ src/store.js
@@ -1,36 +1,50 @@
-import { createStore, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import reducer from "./reducers";
 
-const stringEnhancer =
-  (createStore) =>
-  (...args) => {
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch;
-    store.dispatch = (action) => {
-      if (typeof action === "string") {
-        return originalDispatch({
-          type: action,
-        });
-      }
-      return originalDispatch(action);
-    };
-    return store;
+// мидлваре модифицирует то как работает функция dispatch
+const logMiddleware =
+  ({ getState }) =>
+  (next) =>
+  (action) => {
+    console.log(action.type, getState());
+    return next(action);
   };
 
-const logEnhancer =
-  (createStore) =>
-  (...args) => {
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch;
-    store.dispatch = (action) => {
-      console.log(action.type);
-      return originalDispatch(action);
-    };
-    return store;
-  };
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === "string") {
+    return next({
+      type: action,
+    });
+  }
+  return next(action);
+};
 
-const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+// enhancer такие же как мидлваре, мидлваре специальные функции для такого использования
+// const stringEnhancer =
+//   (createStore) =>
+//   (...args) => {
+//     const store = createStore(...args);
+//     const originalDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//       return originalDispatch(action);
+//     };
+//     return store;
+//   };
+
+// const logEnhancer =
+//   (createStore) =>
+//   (...args) => {
+//     const store = createStore(...args);
+//     const originalDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//       console.log(action.type);
+//       return originalDispatch(action);
+//     };
+//     return store;
+//   };
+
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
 
 store.dispatch("HELLO");
 
